29f34814ddb45e9295c2a828515deb4ce8bb073d
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-03-02 23:02:34 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FreeMarket
+Blazor WebAssembly application hosted on ASP.NET Core back-end.
+#### Created for SoftUni - C# Web Advanced course.
---
+++ Server/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace OnlineMarket.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBlazorDebugging();
+            }
+
+            app.UseStaticFiles();
+            app.UseClientSideBlazorFiles<Client.Program>();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapFallbackToClientSideBlazor<Client.Program>("index.html");
+            });
+        }
+    }
+}
