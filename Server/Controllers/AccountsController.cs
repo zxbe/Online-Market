6982cdae9ee44658d5041d5d04af455ebafb1aac
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-03-04 20:02:47 +0200

    .Server - Added - AccountsController - skeleton.

---
+++ Server/Controllers/AccountsController.cs
@@ -0,0 +1,42 @@
+﻿namespace OnlineMarket.Server.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route(ControllersConstants.ControllerRoute)]
+    public class AccountsController : ControllerBase
+    {
+        private const string ProlifeRouteTemplate = "Profile";
+
+        [HttpPost(ControllersConstants.ActionRoute)]
+        public async Task<IActionResult> Register()
+        {
+            return this.Ok();
+        }
+
+        [HttpPost(ControllersConstants.ActionRoute)]
+        public async Task<IActionResult> Login()
+        {
+            return this.Ok();
+        }
+
+        [HttpGet(ProlifeRouteTemplate)]
+        public async Task<IActionResult> GetProfile()
+        {
+            return this.Ok();
+        }
+
+        [HttpPut(ProlifeRouteTemplate)]
+        public async Task<IActionResult> UpdateProfile()
+        {
+            return this.Ok();
+        }
+
+        [HttpDelete(ProlifeRouteTemplate)]
+        public async Task<IActionResult> DeleteProfile()
+        {
+            return this.Ok();
+        }
+    }
+}
---
+++ Server/Controllers/Constants/ControllersConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineMarket.Server.Controllers
+{
+    public class ControllersConstants
+    {
+        public const string ControllerRoute = "[controller]";
+        public const string ActionRoute = "[action]";
+    }
+}
