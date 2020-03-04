3f640f2667e93465d0a3d584d5daae6b40287e4d
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-03-04 21:54:36 +0200

    - Added - BaseResult to .Shared/Results

---
+++ Shared/Results/BaseResult.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineMarket.Shared.Results
+{
+    using System.Collections.Generic;
+
+    public class BaseResult
+    {
+        public bool IsSuccessful { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
