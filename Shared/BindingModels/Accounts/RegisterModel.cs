e8ec6d66156d75fbbfcec0bb67a60c567f6a2f74
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-03-05 10:46:05 +0200

    - Added - RegisterModel to .Shared/BindingModels/Accounts. - Added - CommonConstants to .Shared/BindingModels

---
+++ Shared/BindingModels/Accounts/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿namespace OnlineMarket.Shared.BindingModels.Accounts
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterModel
+    {
+        private const int UsernameMinLength = 6;
+        private const int UsernameMaxLength = 50;
+
+        [Required]
+        [StringLength(UsernameMaxLength,
+            ErrorMessage = CommonConstants.StringLengthErrorMessage,
+            MinimumLength = UsernameMinLength)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password),
+            ErrorMessage = CommonConstants.CompareErrorMessage)]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Shared/BindingModels/CommonConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineMarket.Shared.BindingModels
+{
+    public class CommonConstants
+    {
+        public const string StringLengthErrorMessage = "{0} must be between {2} and {1} symbols long.";
+        public const string CompareErrorMessage = "{1} and {0} does not match";
+    }
+}
