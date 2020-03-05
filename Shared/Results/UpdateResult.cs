62724cd7e5b7194d23a7014975ee63e90f54e592
Hristo Nikodimov Nenkov <hristo.nikodimov.nenkov@gmail.com>
2020-03-05 10:28:11 +0200

    - Added - UpdateResult<T> to .Shared/Results

---
+++ Shared/Results/UpdateResult.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineMarket.Shared.Results
+{
+    public class UpdateResult<T> : BaseResult
+    {
+        public T PreviousObject { get; set; }
+        public T UpdatedObject { get; set; }
+    }
+}
